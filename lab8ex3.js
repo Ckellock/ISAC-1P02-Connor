61c4a9b9911d8034ff367119d8eb8c03f14edf3d
Ckellock <connorkellock@gmail.com>
2019-11-08 13:00:13 -0500

    lab 8 challenge code

---
+++ lab8ex3.js
@@ -0,0 +1,10 @@
+//This will output when the page loads
+
+
+//This will output when a button is pressed
+function theRest(){
+  document.getElementById("output").innerHTML = "0"
+  document.getElementById("output1").innerHTML = "1";
+  document.getElementById("output2").innerHTML = "2";
+document.getElementById("output3").innerHTML="3";
+}
