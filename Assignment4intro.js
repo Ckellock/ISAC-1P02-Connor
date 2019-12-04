6f1bfe72da60ef8f00ef8cf5441cb74564a8f129
Ckellock <55201587+Ckellock@users.noreply.github.com>
2019-12-03 20:27:06 -0500

    Assignment 4 Website

---
+++ Assignment4intro.js
@@ -0,0 +1,24 @@
+function  startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('estHeader').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+function giveName(){
+  var name = prompt("What is your name?");
+  document.getElementById("yourName").style.textAlign = "center";
+  document.getElementById("yourName").innerHTML = name;
+}
+function redirectPage() {
+  var ans = confirm( "You are going to another page whether you like it or not!");
+}
---
+++ assignment4AboutMe.js
@@ -0,0 +1,19 @@
+function  startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('estHeader').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  
+  return i;
+}
+
+function redirectPage() {
+  var ans = confirm( "You are going to another page whether you like it or not!");
+}
